7c36d2eaa5b1ee96ffa39695401e68bbc37de235
Hanefi Onaldi <hanefionaldi@gmail.com>
2018-01-02 16:24:38 +0300

    added setup scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TurkishStemmer',
+      version='1.0',
+      description='Turkish Stemmer',
+      author='Hanefi Önaldı',
+      author_email='abdullahanefi16@gmail.com',
+      url='https://github.com/hanefi/turkish-stemmer-python',
+      packages=['TurkishStemmer'],
+     )
