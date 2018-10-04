ea1df9f8529afbe42d5946771fc8222635e2f1d3
Hanefi Onaldi <hanefionaldi@gmail.com>
2018-10-04 16:29:07 +0300

    added missing modules to pypi configs

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(name='TurkishStemmer',
-      version='1.0',
-      description='Turkish Stemmer',
-      author='Hanefi Önaldı',
-      author_email='abdullahanefi16@gmail.com',
-      url='https://github.com/hanefi/turkish-stemmer-python',
-      packages=['TurkishStemmer'],
-     )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='TurkishStemmer',
+    version='1.1',
+    description='Turkish Stemmer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Hanefi Önaldı',
+    author_email='abdullahanefi16@gmail.com',
+    url='https://github.com/hanefi/turkish-stemmer-python',
+    packages=setuptools.find_packages(),
+   )
