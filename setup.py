ce5e6307cff4395da7725ca6b7a1d8276a19d023
hanefi <hanefionaldi@gmail.com>
2018-10-09 16:20:25 +0300

    upload package to PyPI
    added setup scripts

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='TurkishStemmer',
+    version='1.3',
+    description='Turkish Stemmer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Hanefi Onaldi',
+    author_email='abdullahanefi16@gmail.com',
+    url='https://github.com/hanefi/turkish-stemmer-python',
+    packages=setuptools.find_packages(),
+    package_data={'':['*.txt'],},
+   )
